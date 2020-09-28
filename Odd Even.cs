6e7738a11474b216a2f78d62675a9303dbf196ff
Jeya-jp <71859343+Jeya-jp@users.noreply.github.com>
2020-09-28 20:48:41 +0530

    Add files via upload

---
+++ Odd Even.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Odd_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter the number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " is an even number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is an odd number.");
+            }
+            Console.ReadKey();
+        }
+    }
+}

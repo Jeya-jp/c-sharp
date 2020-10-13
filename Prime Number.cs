ecde665b262ebd8ba2f8c39be6d2c4c8b1c7c9d0
Jeya-jp <71859343+Jeya-jp@users.noreply.github.com>
2020-10-13 22:10:09 +0530

    Add files via upload

---
+++ Prime Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, count=0;
+            Console.Write("Enter the number: ");
+            number = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine(number + " is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a prime number.");
+            }
+            Console.ReadKey();
+        }
+    }
+}

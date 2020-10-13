6df83d25a6bce9e76c7b2693fbd8f22a05714cea
Jeya-jp <71859343+Jeya-jp@users.noreply.github.com>
2020-10-13 22:23:52 +0530

    Add files via upload

---
+++ Fibonacci Series.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = -1, n2 = 1, n3, number;
+            Console.Write("Enter the number: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write("The Fibonacci Series is ");
+            for (int i = 0; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write("\n" + n3);
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadKey();
+        }
+    }
+}

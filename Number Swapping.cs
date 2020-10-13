fe931171c61a4ef46b7df2d9b76a56be17d3a1c6
Jeya-jp <71859343+Jeya-jp@users.noreply.github.com>
2020-10-13 22:15:27 +0530

    Add files via upload

---
+++ Number Swapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.Write("Enter the 1st value: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Enter the 2nd value: ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before Swapping:");
+            Console.WriteLine("X value = " + x);
+            Console.WriteLine("Y value = " + y);
+            Console.WriteLine("After Swapping:");
+            z = y;
+            y = x;
+            Console.WriteLine("X value = " + z);
+            Console.WriteLine("Y value = " + y);
+            Console.ReadKey();
+        }
+    }
+}

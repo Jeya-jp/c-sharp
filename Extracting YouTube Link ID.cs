21aa4447fd86ecbf00b2b6f96f7e24e6a36818b3
Jeya-jp <71859343+Jeya-jp@users.noreply.github.com>
2020-10-13 22:27:11 +0530

    Add files via upload

---
+++ Extracting YouTube Link ID.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Extract_ID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code only for YouTube Link
+            // Eg: Short url: https://youtu.be/fP6G8ca0aU4
+            // Eg: Long url: https://www.youtube.com/watch?v=fP6G8ca0aU4
+            string url, result, final;
+            Console.Write("Enter the short url: ");
+            url = Console.ReadLine();
+            result = url.Substring(url.LastIndexOf("/"));
+            final = result.Substring(1);
+            Console.WriteLine("The video ID is: "+ final);
+            Console.Write("Enter the long url: ");
+            url = Console.ReadLine();
+            result = url.Substring(url.LastIndexOf("="));
+            final = result.Substring(1);
+            Console.Write("The video ID is: " + final);
+            Console.ReadKey();
+        }
+    }
+}
